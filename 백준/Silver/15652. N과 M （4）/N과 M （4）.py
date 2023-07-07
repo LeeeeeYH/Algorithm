113a669da0102f36da5c9dc5e7d81ba8559b798f
LeeeeeYH <123916208+LeeeeeYH@users.noreply.github.com>
2023-07-07 17:20:55 +0900

    [Silver III] Title: N과 M (4), Time: 192 ms, Memory: 118040 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+def recur(cur, start, nums):
+    if cur == M:
+        print(*nums)
+        return
+
+    for i in range(start, N+1):
+        recur(cur+1, i, nums+[i])
+
+recur(0, 1, [])
