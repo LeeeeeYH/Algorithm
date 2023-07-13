e45266c37f6b6e86b2b9e00f3d9897e1531f3d5b
LeeeeeYH <123916208+LeeeeeYH@users.noreply.github.com>
2023-07-14 08:26:25 +0900

    [Silver III] Title: 퇴사, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,8 +1,11 @@
+# DP로 변경
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 ls = [list(map(int, input().split())) for _ in range(N)]
+dp = [-1] * N
 
 def recur(cur):
     if cur > N:
@@ -10,6 +13,8 @@ def recur(cur):
     if cur == N:
         return 0
 
-    return max(recur(cur+ls[cur][0]) + ls[cur][1], recur(cur+1))
+    if dp[cur] == -1:
+        dp[cur] = max(recur(cur + ls[cur][0]) + ls[cur][1], recur(cur + 1))
+    return dp[cur]
 
 print(recur(0))
