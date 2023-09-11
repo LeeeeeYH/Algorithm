6a51b4a8649006f90416c30b029f0818d800aa91
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2023-09-11 17:57:16 +0900

    [Gold IV] Title: 수들의 합 4, Time: 208 ms, Memory: 153024 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,16 +1,18 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-sum_dict = dict()
-sum_num = 0
-res = 0
-for i in A:
-    sum_num += i
-    if sum_num == K: res += 1
-    if sum_dict.get(sum_num - K) is not None:
-        res += sum_dict.get(sum_num - K)
+pre_dict = defaultdict(int)
+pre_dict[0] = 1
 
-    if sum_dict.get(sum_num) is None:
-        sum_dict[sum_num] = 1
-    else:
-        sum_dict[sum_num] += 1
-print(res)
+adding = 0
+count = 0
+
+for i in A:
+    adding += i
+    count += pre_dict[adding - K]
+    pre_dict[adding] += 1
+
+print(count)
