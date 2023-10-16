35dec4219694c363d278d7ec0ff705fd979467bf
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2023-10-16 16:09:45 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * 백준 문제의 기본 class 이름은 Main이므로 main함수를 단축키로 사용시 두 번째 것을 사용한다.
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		
+		
+	} // end of main
+} // end of class
