749d993c9d945fb63caa9755062b2777bbf8637e
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2023-11-21 12:47:54 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 520 ms, Memory: 122052 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,31 @@
+N, K = map(int, input().split())
+
+if N == K:
+    print(0)
+    print(N)
+    exit(0)
+
+q = []
+ls = [-1] * 100001
+res = []
+
+q.append(N)
+ls[N] = N
+
+while True:
+    cur = q.pop(0)
+    if cur == K:
+        res.append(cur)
+        p = ls[cur]
+        while p != ls[p]:
+            res.append(p)
+            p = ls[p]
+        res.append(p)
+        print(len(res)-1)
+        print(*res[::-1])
+        break
+
+    for next in [cur-1, cur+1, cur*2]:
+        if 0 <= next <= 100000 and ls[next] == -1:
+            q.append(next)
+            ls[next] = cur
