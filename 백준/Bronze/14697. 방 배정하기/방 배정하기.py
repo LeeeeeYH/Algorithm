a2daeebbf4df2bcb8ac502bd36aa20eb7a1554a5
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2023-11-21 14:15:56 +0900

    [Bronze II] Title: 방 배정하기, Time: 132 ms, Memory: 108100 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -5,15 +5,13 @@ A, B, C, N = map(int, input().split())
 visited = [False] * (N+1)
 
 def recur(num):
-    if num < 0:
-        return
     if num == 0:
         print(1)
         exit(0)
 
     for i in [A, B, C]:
         next = num - i
-        if not visited[next]:
+        if next >= 0 and not visited[next]:
             visited[next] = True
             recur(next)
 
