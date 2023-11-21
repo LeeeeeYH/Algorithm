b12bb2be6de506174b77001fb010ac7e8848f394
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2023-11-21 15:44:11 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 113152 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -2,13 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+num = [0] * M
 
-def recur(cur, start, nums):
+def recur(cur, start):
     if cur == M:
-        print(*nums)
+        print(*num)
         return
 
-    for i in range(start, N+1):
-        recur(cur+1, i, nums+[i])
+    for i in range(start, N + 1):
+        num[cur] = i
+        recur(cur + 1, i)
 
-recur(0, 1, [])
+recur(0, 1)
