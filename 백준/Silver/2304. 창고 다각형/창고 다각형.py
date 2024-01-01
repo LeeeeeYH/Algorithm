0e8a11f945c7e9ec060cee8aba1c2b4887f3ef3c
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-01 17:16:57 +0900

    [Silver II] Title: 창고 다각형, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -2,19 +2,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-pillars = [0]*1001
+pillars = [0]*1002
 for _ in range(N):
     L, H = map(int, input().split())
     pillars[L] = H
 
-left = [0]*1001
-left[0] = pillars[0]
-right = [0]*1001
-right[1000] = pillars[1000]
+left = [0]*1002
+right = [0]*1002
 
 for i in range(1, 1001):
     left[i] = max(left[i-1], pillars[i])
-for i in range(999, -1, -1):
+for i in range(1000, -1, -1):
     right[i] = max(right[i+1], pillars[i])
 
 res = 0
