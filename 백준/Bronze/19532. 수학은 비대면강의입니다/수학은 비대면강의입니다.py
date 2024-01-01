9e054af2a39a58787b52760f4495e9b098d8195e
KeyPla <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-01 14:42:18 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 128 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-a, b, c, d, e, f = map(int, input().split())
+a,b,c,d,e,f = map(int, input().split())
 
 for x in range(-999, 1000):
     for y in range(-999, 1000):
