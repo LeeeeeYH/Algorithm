562ec91b11fedee7899e38839ca3eb741a985849
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-02 16:49:31 +0900

    [Silver V] Title: 유미, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/17286. 유미/유미.py
+++ 백준/Silver/17286. 유미/유미.py
@@ -6,7 +6,7 @@ xy = list(map(int, input().split()))
 ls = [list(map(int, input().split())) for _ in range(3)]
 
 def dis(a, b):
-    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
 
 print(int(min(dis(xy, ls[0])+dis(ls[0], ls[1])+dis(ls[1], ls[2]),
               dis(xy, ls[0])+dis(ls[0], ls[2])+dis(ls[2], ls[1]),
