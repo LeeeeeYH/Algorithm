cda855d03c150e28a9a6c2b569b00e47d5c26ad9
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-07 16:32:35 +0900

    [Silver I] Title: 정수 좌표의 개수, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.py
+++ 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.py
@@ -2,25 +2,25 @@ import sys
 input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
+res = 0
 
 def gcd(a, b):
-    while b != 0:
+    while b:
         a, b = b, a%b
     return a
 
-res = 0
+# 가로세로 아닌 선분의 (서로소인)기울기로 구하기
+for x in range(1, N+1):
+    for y in range(1, M+1):
+        if gcd(x, y) == 1:  # 기울기 x, y가 서로소 -> 선분의 점이 2개
+            xk, yk = x*(K-1), y*(K-1)  # 점이 K개인 선분
+            if xk <= N and yk <= M:
+                res += (N - xk + 1)*(M - yk + 1)
 
-# 기울기있는 선분 구하기
-for dx in range(1, N+1):
-    for dy in range(1, M+1):
-        if gcd(dx, dy) == 1: # 서로소인 것만
-            lx, ly = dx*(K-1), dy*(K-1) # 선분 x, y 길이
-            if lx <= N and ly <= M:
-                res += (N-lx+1)*(M-ly+1)
-
-# 음수 기울기 계산
+# 음수 기울기
 res *= 2
-# x축, y축에 평행한 선분 개수
-res += max(0, (N+1)*(M+2-K)) + max(0, (M+1)*(N+2-K))
+
+# 가로선분, 세로선분
+res += max(0, (M+1)*(N-K+2)) + max(0, (N+1)*(M-K+2))
 
 print(res)
