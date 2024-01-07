c5860784c55a96dda7dc706d9c0c8bd37ac4f461
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-07 14:39:24 +0900

    [Silver IV] Title: 최대 GCD, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.py
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -1,15 +1,17 @@
-# 최대 GCD
-def GCD(a, b): # a >= b 보장
-    while a % b:
-        a, b = b, a % b
-    return b
+import sys
+input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-    nums = sorted(list(map(int, input().split())))
-    leng = len(nums)
-    max_GCD = 0
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+N = int(input())
+for _ in range(N):
+    ls = list(map(int, input().split()))
+    leng = len(ls)
+    res = 0
     for i in range(leng-1):
         for j in range(i+1, leng):
-            max_GCD = max(max_GCD, GCD(nums[j], nums[i]))
-    print(max_GCD)
+            res = max(res, gcd(ls[i], ls[j]))
+    print(res)
