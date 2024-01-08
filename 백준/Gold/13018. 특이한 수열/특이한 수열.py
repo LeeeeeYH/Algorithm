7c352f322fb749a7a528ddf1357cf8e2ada1e25a
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-08 17:09:53 +0900

    [Gold IV] Title: 특이한 수열, Time: 148 ms, Memory: 115408 KB -BaekjoonHub

--- 백준/Gold/13018. 특이한 수열/특이한 수열.py
+++ 백준/Gold/13018. 특이한 수열/특이한 수열.py
@@ -9,11 +9,9 @@ if one == 0:  # 1때문에 모두 만족하지 않을 순 없음
 elif one % 2 == 0:  # one만큼 두개씩 바꾸고 나머지 그대로
     for i in range(one//2):
         print(i*2+2, i*2+1, end=" ")
-    for i in range(one+1, n+1):
-        print(i, end=" ")
+    print(*range(one+1, n+1))
 else: # 1은 그대로, 2부터 두개씩 바꾸고 나머지 그대로
     print(1, end=" ")
     for i in range(one//2):
         print(i*2+3, i*2+2, end=" ")
-    for i in range(one+1, n+1):
-        print(i, end=" ")
+    print(*range(one+1, n+1))
