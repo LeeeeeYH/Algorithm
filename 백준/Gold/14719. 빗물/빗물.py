e6f7dd90027b3ace2361257166bfbe888d53e1ef
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-13 16:29:24 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,27 +2,15 @@ import sys
 input = sys.stdin.readline
 
 H, W = map(int, input().split())
-blocks = list(map(int, input().split()))
+ls = list(map(int, input().split()))
+
+left, right = [ls[0]] + [0]*(W-1), [0]*(W-1) + [ls[W-1]]
+for i in range(1, W):
+    left[i] = max(left[i-1], ls[i])
+for i in range(W-2, -1, -1):
+    right[i] = max(right[i+1], ls[i])
 
 res = 0
-high = blocks[0]
-water = 0
-for i in range(1, W):
-    if blocks[i] < high:
-        water += high-blocks[i]
-    else:
-        res += water
-        water = 0
-        high = blocks[i]
-
-high = blocks[W-1]
-water = 0
-for i in range(W-2, -1, -1):
-    if blocks[i] < high:
-        water += high-blocks[i]
-    elif blocks[i] > high:
-        res += water
-        water = 0
-        high = blocks[i]
-
+for i in range(W):
+    res += min(left[i], right[i]) - ls[i]
 print(res)
