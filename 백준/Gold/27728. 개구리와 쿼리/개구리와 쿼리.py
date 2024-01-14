3338616c1741e43adaaf6c80953cb6a2b464fb07
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-14 16:56:21 +0900

    [Gold I] Title: 개구리와 쿼리, Time: 772 ms, Memory: 115932 KB -BaekjoonHub

--- 백준/Gold/27728. 개구리와 쿼리/개구리와 쿼리.py
+++ 백준/Gold/27728. 개구리와 쿼리/개구리와 쿼리.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N, Q = map(int, input().split())
-A = [list(map(int, input().split())) + [0] for _ in range(N)]  # 오른쪽까지 가기위한 누적합
-min_A = [[0]*N for _ in range(N)]  # 이보다 위의 좌표에 대해 오른쪽까지 가기 위한 누적값의 최솟값
+A = [list(map(int, input().split())) + [0] for _ in range(N)]
+min_A = [[0]*N for _ in range(N)]
 for i in range(N):
     for j in range(N-2, -1, -1):
         A[i][j] += A[i][j+1]
@@ -16,7 +16,7 @@ for i in range(1, N):
 for _ in range(Q):
     Sx, Sy, L = map(int, input().split())
     Sx, Sy = Sx-1, Sy-1
-    res = A[Sx][Sy]  # 점프없이 가기
-    for i in range(Sy, N):  # i번째에서 점프하기
+    res = A[Sx][Sy]
+    for i in range(Sy, N):
         res = min(res, A[Sx][Sy]-A[Sx][i] + min_A[Sx-L][i])
     print(res)
