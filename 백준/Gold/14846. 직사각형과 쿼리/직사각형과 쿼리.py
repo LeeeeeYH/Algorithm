c8e05272b931c6acd32d944451d1b47cb94ba878
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-14 14:30:50 +0900

    [Gold IV] Title: 직사각형과 쿼리, Time: 336 ms, Memory: 127668 KB -BaekjoonHub

--- 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.py
+++ 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.py
@@ -3,23 +3,18 @@ input = sys.stdin.readline
 
 N = int(input())
 board = [[0]*(N+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
-pre_sum = [[[0]*11 for _ in range(N+1)] for _ in range(N+1)] # index의 번호가 존재하는 갯수
-
+pre = [[[0]*11 for _ in range(N+1)] for _ in range(N+1)]  # i, j까지 k가 몇개 있나
 for i in range(1, N+1):
     for j in range(1, N+1):
+        pre[i][j][board[i][j]] += 1
         for k in range(1, 11):
-            pre_sum[i][j][k] = pre_sum[i-1][j][k] + pre_sum[i][j-1][k] - pre_sum[i-1][j-1][k]
-        pre_sum[i][j][board[i][j]] += 1
+            pre[i][j][k] += pre[i-1][j][k] + pre[i][j-1][k] - pre[i-1][j-1][k]
 
 Q = int(input())
 for _ in range(Q):
     x1, y1, x2, y2 = map(int, input().split())
-    have_arr = [0]*11
-    for k in range(1, 11):
-        have_arr[k] = pre_sum[x2][y2][k] - pre_sum[x1-1][y2][k] - pre_sum[x2][y1-1][k] + pre_sum[x1-1][y1-1][k]
-
     res = 0
     for k in range(1, 11):
-        if have_arr[k] > 0:
+        if pre[x2][y2][k] - pre[x1-1][y2][k] - pre[x2][y1-1][k] + pre[x1-1][y1-1][k]:
             res += 1
     print(res)
