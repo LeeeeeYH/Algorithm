680d0956dc6ebee89b6226559f668a36467885de
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-14 13:58:23 +0900

    [Gold III] Title: 행렬 찾기, Time: 216 ms, Memory: 112320 KB -BaekjoonHub

--- 백준/Gold/1687. 행렬 찾기/행렬 찾기.py
+++ 백준/Gold/1687. 행렬 찾기/행렬 찾기.py
@@ -2,21 +2,21 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-board = [[0]*(M+1)] + [[0]+[int(i) for i in input().rstrip()] for _ in range(N)]
-pre_sum = [[0]*(M+1) for _ in range(N+1)]
+board = [[0]*(M+1)] + [[0] + [int(i) for i in input().rstrip()] for _ in range(N)]
+pre_board = [[0]*(M+1) for _ in range(N+1)]  # 이거 포함 바로 위로 몇개나 0인지
 
 for i in range(1, N+1):
     for j in range(1, M+1):
         if board[i][j] == 0:
-            pre_sum[i][j] = pre_sum[i-1][j] + 1
+            pre_board[i][j] = pre_board[i-1][j] + 1
 
 res = 0
 for i in range(1, N+1):
     for j in range(1, M+1):
-        if pre_sum[i][j] != 0:
-            min_len = 333
-            for k in range(j, 0, -1):
-                min_len = min(min_len, pre_sum[i][k])
-                res = max(res, (j-k+1) * min_len)
+        if pre_board[i][j]:  # 0인 부분이 존재
+            can_h = pre_board[i][j]  # 가능한 높이
+            for k in range(j+1, M+1):
+                can_h = min(can_h, pre_board[i][k])
+                res = max(res, can_h*(k-j+1))
 
 print(res)
