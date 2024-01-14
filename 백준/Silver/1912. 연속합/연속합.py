598107cbbeaccc0d7e82614de711baa44ff33d94
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-14 15:59:53 +0900

    [Silver II] Title: 연속합, Time: 132 ms, Memory: 122524 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,14 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [0] + list(map(int, input().split()))
-pre_sum = [0]*(n+1)
-min_sum = 0
-res = arr[1]
-
-for i in range(1, n+1):
-    pre_sum[i] = arr[i] + pre_sum[i-1]
-    res = max(res, pre_sum[i] - min_sum)
-    min_sum = min(min_sum, pre_sum[i])
+ls = [0] + list(map(int, input().split()))
+mini, summ = 0, 0
+res = -100000*1000
+for i in ls[1:]:
+    summ += i
+    res = max(res, summ - mini)
+    mini = min(mini, summ)
 
 print(res)
