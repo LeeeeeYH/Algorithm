2518ff0fbd66d9c3a0b50c9b08e7c1cb8295d74f
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-14 15:50:24 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 452 ms, Memory: 126564 KB -BaekjoonHub

--- 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
@@ -8,14 +8,15 @@ W, H, E = [0]*(N+1), [], [0]*(N+1)
 for i in range(1, N):
     b = N-i  # 뒤에서부터 index
     W[i], E[b] = W[i-1], E[b+1]
-    if S[i] == 'W':
+    if S[i] == 'W':  # 앞에서부터 W가 몇개있냐
         W[i] += 1
-    if S[i] == 'H':
+    if S[i] == 'H':  # H의 index들
         H.append(i)
-    if S[b] == 'E':
+    if S[b] == 'E':  # 뒤에서부터 E가 몇개있냐
         E[b] += 1
 
 res = 0
+# H기준으로 왼쪽에 W개수 X 오른쪽에 E중 2개를 뽑는 경우의수(부분집합 - 한개만 - 0개뽑을때)
 for h in H:
     res += W[h]*(2**E[h] - E[h] - 1)
 print(res%1_000_000_007)
