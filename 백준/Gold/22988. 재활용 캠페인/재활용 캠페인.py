2e39ed4fd7baeb3d0106b3a4f5010e2053d6b424
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-17 16:42:04 +0900

    [Gold II] Title: 재활용 캠페인, Time: 184 ms, Memory: 128788 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
@@ -2,25 +2,25 @@ import sys
 input = sys.stdin.readline
 
 N, X = map(int, input().split())
-Ci = sorted(map(int, input().split()))
+C = sorted(map(int, input().split()))
 
+# 풀병 거르기
 l, r = 0, N-1
-cnt = 0
+res = 0
 for i in range(N-1, -1, -1):
-    if Ci[i] >= X:
+    if C[i] >= X:
         r -= 1
-        cnt += 1
-    else:
-        break
-rest = r+1
+        res += 1
+    else: break
 
+rest = r+1  # 나중에 계산할 남은병 개수
+
+# 3개를 모으면 무조건 풀병
 while l < r:
-    if Ci[l] + Ci[r] >= X/2:
-        l += 1
-        r -= 1
-        rest -= 2
-        cnt += 1
+    if C[l] + C[r] >= X/2:
+        l, r = l+1, r-1
+        res, rest = res+1, rest-2
     else:
         l += 1
 
-print(cnt + rest//3)
+print(res + rest//3)
