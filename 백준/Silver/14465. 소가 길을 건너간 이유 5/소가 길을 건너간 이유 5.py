5dc3dae383dbf26cafa260c2423bcc769ea913e1
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-17 15:47:31 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 116 ms, Memory: 110672 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N, K, B = map(int, input().split())
-ls = [0] * (N+1)  # 0: 멀쩡한 신호등, 1: 고장난 신호등
-for i in range(B):
-    ls[int(input())] = 1
+ls = [1] * N
+for _ in range(B):
+    ls[int(input()) - 1] = 0
 
-s = sum(ls[1:K+1])  # 1~K까지 고장난 신호등의 개수로 초기화
-res = s
-
-for i in range(K+1, N+1):
-    s += ls[i] - ls[i-K]
-    res = min(res, s)
+summ = sum(ls[:K])
+res = K-summ
+for i in range(K, N):
+    summ += ls[i] - ls[i-K]
+    res = min(res, K-summ)
 print(res)
