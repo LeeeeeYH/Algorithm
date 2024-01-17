43f6197fefe7956bf40bb874687c1e228919220e
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-17 15:03:06 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,18 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-a = sorted([int(input()) for _ in range(9)])
-# 9C7 -> 9C2 == 전체합 - 100 인거찾기
-hap = sum(a) - 100
-
+ls = sorted([int(input()) for _ in range(9)])
+summ = sum(ls) - 100
 i, j = 0, 8
 while i < j:
-    tmp = a[i] + a[j]
-    if tmp < hap:
+    if ls[i] + ls[j] < summ:
         i += 1
-    elif tmp > hap:
+    elif ls[i] + ls[j] > summ:
         j -= 1
     else:
-        break
-
-print(*(a[:i] + a[i+1:j] + a[j+1:]), sep='\n')
+        print(*(ls[:i]+ls[i+1:j]+ls[j+1:]), sep="\n")
+        exit(0)
