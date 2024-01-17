573adbb7d1dc7892cdbd41dfc4c1c34d6ae2872d
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-17 15:41:04 +0900

    [Gold V] Title: 다이어트, Time: 116 ms, Memory: 110556 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.py
+++ 백준/Gold/1484. 다이어트/다이어트.py
@@ -2,19 +2,16 @@ import sys
 input = sys.stdin.readline
 
 G = int(input())
-ls = [i*i for i in range(50_002)]
 res = []
-for i in range(1, 50_001):
-    if ls[i+1] - ls[i] > G:
-        break
-    for j in range(i+1, 50_002):
-        diff = ls[j] - ls[i]
-        if diff > G:
+for i in range(1, 50001):
+    for j in range(i+1, 50002):
+        sub = j*j - i*i
+        if sub > G:
             break
-        elif diff == G:
+        elif sub == G:
             res.append(j)
-            break
-if len(res):
+
+if res:
     print(*res, sep="\n")
 else:
     print(-1)
