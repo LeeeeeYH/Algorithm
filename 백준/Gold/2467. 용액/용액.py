cad4feda6954411acfd7f5e401a20f238d499085
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-17 15:19:21 +0900

    [Gold V] Title: 용액, Time: 152 ms, Memory: 125436 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -5,16 +5,14 @@ N = int(input())
 ls = list(map(int, input().split()))
 
 i, j = 0, N-1
-res = abs(ls[i] + ls[j])
-resi, resj = 0, N-1
+resi, resj, res = 0, N-1, 2_000_000_000
 while i < j:
-    if res > abs(ls[i] + ls[j]):
-        res = abs(ls[i] + ls[j])
-        resi, resj = i, j
-
+    summ = abs(ls[i]+ls[j])
+    if res > summ:
+        resi, resj, res = i, j, summ
     if abs(ls[i+1] + ls[j]) < abs(ls[i] + ls[j-1]):
         i += 1
     else:
         j -= 1
 
-print(f'{ls[resi]} {ls[resj]}')
+print(ls[resi], ls[resj])
