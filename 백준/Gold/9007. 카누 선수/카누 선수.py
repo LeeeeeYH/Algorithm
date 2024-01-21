3ed9d7415b36b1a41129e02d7c323c5e84e1dd68
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-21 15:07:01 +0900

    [Gold II] Title: 카누 선수, Time: 4480 ms, Memory: 411312 KB -BaekjoonHub

---
+++ 백준/Gold/9007. 카누 선수/카누 선수.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    k, n = map(int, input().split())
+    ls = [sorted(map(int, input().split())) for _ in range(4)]
+
+    A, B = set(), set()
+    for i in range(n):
+        for j in range(n):
+            A.add(ls[0][i]+ls[1][j])
+            B.add(ls[2][i]+ls[3][j])
+
+    A = sorted(list(A))
+    B = sorted(list(B))
+
+    leng = len(A)
+    j = len(B)-1
+    res, resdis = 0, 40_000_000
+    for i in range(leng):
+        while j >= 0:
+            summ = A[i] + B[j]
+            dis = abs(k - summ)
+            if resdis > dis:
+                res, resdis = summ, dis
+            elif resdis == dis and res > summ:
+                res = summ
+
+            if k < summ:
+                j -= 1
+            else:
+                break
+
+    print(res)
