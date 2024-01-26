bc94f7ec0607d01da7bca8bb67762f011e5a2912
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-01-26 17:18:48 +0900

    [Gold IV] Title: 공유기 설치, Time: 192 ms, Memory: 158888 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -2,22 +2,27 @@ import sys
 input = sys.stdin.readline
 
 N, C = map(int, input().split())
-houses = sorted([int(input()) for _ in range(N)])
+ls = sorted([int(input()) for _ in range(N)])
 
-i, j = 1, 1_000_000_000
+# 최소 거리를 x라고 했을때 가능한 최대 공유기의 수
+def count(x):
+    ret = 1
+    installed = ls[0]
+    for i in ls[1:]:
+        if i - installed >= x:
+            installed = i
+            ret += 1
+    return ret
+
+s, e = 1, 1_000_000_000
 res = 0
-while i <= j:
-    mid = (i+j)//2
-    cnt = 1 # 공유기 설치할 집 수
-    left = houses[0] # 계속 움직이며 거리 계산할 왼쪽 집
-    for house in houses[1:]:
-        if house - left >= mid: # (설치할-이전설치한)집 거리가 mid보다 크면
-            cnt += 1
-            left = house
-    if cnt >= C: # 설치 가능
-        res = mid
-        i = mid + 1
-    else:
-        j = mid - 1
 
+while s <= e:
+    mid = (s+e)//2
+    cnt = count(mid)
+    if cnt >= C:
+        res = mid
+        s = mid+1
+    else:
+        e = mid-1
 print(res)
