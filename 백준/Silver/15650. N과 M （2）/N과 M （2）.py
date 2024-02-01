43450369b2588ca420cbc2a5a9a1bebf72e58347
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-01 15:23:59 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,13 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+num = [0] * M
 
-def recur(cur, start, nums):
+def recur(cur, start, check):
     if cur == M:
-        print(*nums)
+        print(*num)
         return
 
     for i in range(start, N+1):
-        recur(cur+1, i+1, nums+[i])
+        if not check & (1<<i):
+            num[cur] = i
+            recur(cur+1, i + 1, check|1<<i)
 
-recur(0, 1, [])
+recur(0, 1, 0)
