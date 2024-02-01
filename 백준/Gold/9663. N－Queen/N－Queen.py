e32c1aab4286997677a3419b0f64b81c4f80399d
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-01 16:56:29 +0900

    [Gold IV] Title: N-Queen, Time: 30596 ms, Memory: 202224 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -2,18 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-cols = [0] * N
-res = 0
+cols = [0]*N
 
-def check(idx): # 행 번호, 몇 번째 열에 놓을지
-    for i in range(idx): # 이전까지의 행에 대해서
-        if cols[idx] == cols[i] or abs(idx - i) == abs(cols[idx] - cols[i]):
+def check(n):
+    for i in range(n):
+        if cols[i] == cols[n] or abs(i-n) == abs(cols[i] - cols[n]):
             return False
     return True
 
+res = 0
 def recur(cur):
-    global res
     if cur == N:
+        global res
         res += 1
         return
 
@@ -21,5 +21,6 @@ def recur(cur):
         cols[cur] = i
         if check(cur):
             recur(cur+1)
+
 recur(0)
 print(res)
