89c60515c60ed72f30941708efa9fff2de0a6ab4
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-07 19:33:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 248 ms, Memory: 112276 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,18 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N, S = map(int, input().split())
-nums = list(map(int, input().split()))
+ls = list(map(int, input().split()))
 res = 0
 
 def recur(cur, summ, check):
     if cur == N:
+        global res
         if check and summ == S:
-            global res
             res += 1
         return
 
-    recur(cur+1, summ + nums[cur], check|True)
-    recur(cur+1, summ, check)
+    recur(cur + 1, summ + ls[cur], True)
+    recur(cur + 1, summ, check)
 
 recur(0, 0, False)
 print(res)
