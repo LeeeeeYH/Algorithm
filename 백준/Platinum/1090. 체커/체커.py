23928ec7d9cc99c924501386b1c193d81e0cf8f0
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-13 17:27:03 +0900

    [Platinum IV] Title: 체커, Time: 144 ms, Memory: 110872 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.py
+++ 백준/Platinum/1090. 체커/체커.py
@@ -11,17 +11,13 @@ for _ in range(N):
     Y.append(y)
 res = [100_000_000] * N
 
-def check(x, y):
-    # (x, y)와 가까운 순서대로 저장
-    diss = sorted([abs(a-x) + abs(b-y) for a, b in ls])
-
-    summ = 0
-    for i in range(N):
-        summ += diss[i]
-        res[i] = min(res[i], summ)  # (x, y)에 대해 체커가 i개 일때 최소거리
-
-
 for x in X:
     for y in Y:
-        check(x, y)
+        # (x, y)와 가까운 순서대로 저장
+        diss = sorted([abs(a-x) + abs(b-y) for a, b in ls])
+
+        summ = 0
+        for i in range(N):
+            summ += diss[i]
+            res[i] = min(res[i], summ)  # (x, y)에 대해 체커가 i개 일때 최소거리
 print(*res)
