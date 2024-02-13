439f4d110b7ff58d3d72a5a1e82848896711ea16
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-13 14:48:58 +0900

    [Gold V] Title: 리모컨, Time: 344 ms, Memory: 113332 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,23 +1,23 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 N = int(input())
 M = int(input())
-alive = [True] * 10
-if M != 0:
-    for i in list(map(int, input().split())):
-        alive[i] = False
-buttons = [i for i in range(10) if alive[i]]
-
+ls = []
+if M > 0:
+    ls = list(map(int, input().split()))
+buttons = [i for i in range(10) if i not in ls]
 res = abs(N-100)
-def recur(cur, count):
-    global res
-    res = min(res, abs(cur - N) + count)
 
+def recur(cur, count):
+    disN = abs(cur - N)  # N까지 위아래만 눌러서 갈 때의 거리
+    global res
+    res = min(res, disN + count)  # 여태 누른 번호 + 위아래로 가는 거리
+
+    # 고장 안난 번호 또 누르기
     for i in buttons:
         next = cur*10 + i
-        if abs(next - N) < abs(cur - N):
+        if abs(next - N) < disN:  # 더 적을때만 진행
             recur(next, count+1)
 
 for i in buttons:
