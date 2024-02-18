925e14ad84eebad490fd151d6b066818e2c697a6
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-18 18:03:38 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31276 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,40 +1,21 @@
-# import sys
-# input = sys.stdin.readline
-#
-# N = int(input())
-# ls = [list(map(int, input().split())) for _ in range(N)]
-#
-# def recur(cur, bf):
-#     if cur == N:
-#         return 0
-#
-#     min_now = 10000000
-#     for i in range(3):
-#         if i != bf:
-#             min_now = min(min_now, recur(cur + 1, i) + ls[cur][i])
-#     return min_now
-#
-# print(recur(0, -1))
-
-
-# 이 백트를 DP로 변경
-
 import sys
+sys.setrecursionlimit(10000000)
 input = sys.stdin.readline
 
 N = int(input())
 ls = [list(map(int, input().split())) for _ in range(N)]
-CHECK = 10 ** 6
-dp = [[CHECK] * 3 for i in range(N)]
+dp = [[-1]*3 for _ in range(N)]
 
 def recur(cur, bf):
     if cur == N:
         return 0
 
-    if dp[cur][bf] == CHECK:
+    if dp[cur][bf] == -1:
+        ret = 1_000_000
         for i in range(3):
             if i != bf:
-                dp[cur][bf] = min(dp[cur][bf], recur(cur + 1, i) + ls[cur][i])
+                ret = min(ret, recur(cur+1, i) + ls[cur][i])
+        dp[cur][bf] = ret
     return dp[cur][bf]
 
 print(recur(0, -1))
