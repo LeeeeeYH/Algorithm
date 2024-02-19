012f771b689b233cb193bfdf278cbc495d69ab2d
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-19 14:31:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31296 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -6,31 +6,28 @@
 # def recur(cur):
 #     if cur > n:
 #         return 0
-#     if cur == n:
+#     elif cur == n:
 #         return 1
 #
 #     return recur(cur+1) + recur(cur+2)
 #
-# print(recur(0) % 10007)
-
-
-# 백트를 dp로
+# print(recur(0))
 
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10_000_000)
 input = sys.stdin.readline
 
 n = int(input())
-dp = [-1] * n
+dp = [-1]*1001
 
 def recur(cur):
     if cur > n:
         return 0
-    if cur == n:
+    elif cur == n:
         return 1
 
     if dp[cur] == -1:
-        dp[cur] = recur(cur+1) + recur(cur+2)
+        dp[cur] = (recur(cur+1) + recur(cur+2))%10007
     return dp[cur]
 
-print(recur(0) % 10007)
+print(recur(0))
