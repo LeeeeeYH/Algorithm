0e99cd64700a1e208fb062b8a71ab12aa28f8336
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-02-26 16:53:22 +0900

    [Silver III] Title: 돌 게임 3, Time: 44 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -1,47 +1,34 @@
-# import sys
-# input = sys.stdin.readline
+# input = __import__('sys').stdin.readline
 #
-# N = int(input())
-#
-# def recur(cur):
-#     if cur < 0: # 상대방 잘못
+# def recur(cur):  # 내가 질 수 있는가
+#     if cur < 0:  # 잘못된 입력
 #         return True
-#     if cur == 0: # 상대방이 막타
+#     if cur == 0:  # 내가 짐
 #         return False
 #
-#     canwin = False
-#     for i in [1, 3, 4]:
-#         if not recur(cur - i):
-#             canwin = True
-#             break
+#     # 하나라도 상대방이 지는 상황이 있다면 True여야함
+#     # ==> recur(cur-i)가 하나라도 False면 True를 반환해야함
+#     return not(recur(cur-1) and recur(cur-3) and recur(cur-4))
 #
-#     return canwin
-#
-# print('SK' if recur(N) else 'CY')
-
-# 백트를 dp로
+# print('SK' if recur(int(input())) else 'CY')
 
 import sys
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10_000_000)
 input = sys.stdin.readline
 
 N = int(input())
-check = [False] * 1004
-dp = [False] * 1004
+dp = [-1] * (N+1)
 
-def recur(cur):
-    if cur < 0: # 상대방 잘못
+def recur(cur):  # 내가 질 수 있는가
+    if cur < 0:  # 잘못된 입력
         return True
-    if cur == 0: # 상대방이 막타
+    if cur == 0:  # 내가 짐
         return False
 
-    if not check[cur]: # 계산 안해봤으면
-        check[cur] = True
-        for i in [1, 3, 4]:
-            if not recur(cur - i):
-                dp[cur] = True
-                break
-
+    # 하나라도 상대방이 지는 상황이 있다면 True여야함
+    # ==> recur(cur-i)가 하나라도 False면 True를 반환해야함
+    if dp[cur] == -1:
+        dp[cur] = not(recur(cur-1) and recur(cur-3) and recur(cur-4))
     return dp[cur]
 
 print('SK' if recur(N) else 'CY')
