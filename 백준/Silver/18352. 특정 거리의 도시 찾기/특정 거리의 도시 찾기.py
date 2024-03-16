17efd426bf12023c15c0881949bb4269f14efb5d
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-03-16 13:09:08 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 932 ms, Memory: 150780 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,28 @@
+input = __import__('sys').stdin.readline
+
+dirs = [[-1,0],[0,1],[1,0],[0,-1]]
+N, M, K, X = map(int, input().split())
+edges = [[] for _ in range(N+1)]
+for _ in range(M):
+    A, B = map(int, input().split())
+    edges[A].append(B)
+check = [False]*(N+1)
+
+res = []
+q = [[X, 0]]
+check[X] = True
+while q:
+    cur, w = q.pop(0)
+    if w == K:
+        res.append(cur)
+        continue
+
+    for next in edges[cur]:
+        if not check[next] and w < K:
+            check[next] = True
+            q.append([next, w+1])
+
+if res:
+    print(*sorted(res), sep='\n')
+else:
+    print(-1)
