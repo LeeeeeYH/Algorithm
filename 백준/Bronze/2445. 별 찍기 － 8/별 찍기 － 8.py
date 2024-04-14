d5e4c3f39f9ac109cd980fdc6a6e9623d7979a5c
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-04-14 14:18:02 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -1,5 +1,3 @@
 N = int(__import__('sys').stdin.readline())
 for i in range(2*N-1):
-    print('*'*(N-abs(i-N+1)), end='')
-    print(' '*(2*abs(i-N+1)), end='')
-    print('*'*(N-abs(i-N+1)))
+    print('*'*(N-abs(i-N+1)), ' '*(2*abs(i-N+1)), '*'*(N-abs(i-N+1)), sep='')
