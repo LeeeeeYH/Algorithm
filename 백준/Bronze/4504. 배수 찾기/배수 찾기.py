0b19e81ac6cb7dbd09269a4ea58d7e448908b3a4
YongHyun 용현 <123916208+LeeeeeYH@users.noreply.github.com>
2024-05-24 20:54:41 +0900

    [Bronze III] Title: 배수 찾기, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,12 @@
+input = __import__('sys').stdin.readline
+
+n = int(input())
+while True:
+    num = int(input())
+    if num == 0:
+        break
+
+    if num%n == 0:
+        print(num, " is a multiple of ", n, ".", sep='')
+    else:
+        print(num, " is NOT a multiple of ", n, ".", sep='')
